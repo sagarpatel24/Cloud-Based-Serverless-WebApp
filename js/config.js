f738e23621f95c2399b2e7358d24688f2c345bf1
Sagar <35815533+sagarpatel24@users.noreply.github.com>
2020-05-01 12:02:16 -0400

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_8mFXqChF0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '30pubjkdfd3g85c7id1et8a6p5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://a4nrbwm6ri.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
